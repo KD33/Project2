91308d94264aa0a16b9bdb5b4585876b2f320151
KD33 <Walshk94@gmail.com>
2018-03-03 18:19:03 +0000

    Set up forms for user place submission

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -8,4 +8,17 @@ class PlacesController < ApplicationController
     #make a form for that particular thing
   end
 
+  def create
+    #pressing submit on the form triggers the create action
+    #all the code here will get executed when the user hits create
+    Place.create(place_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def place_params #what pulls the values of name, description and address from the place form
+    params.require(:place).permit(:name, :description, :address)
+  end
+
 end
