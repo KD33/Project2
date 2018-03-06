470017cbe68275067eeb68d073600b6b6f8f8895
KD33 <Walshk94@gmail.com>
2018-03-06 01:44:48 +0000

    Removed comments in controller and html files

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,31 +1,26 @@
 class PlacesController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
   def index
-    @places = Place.all.page params[:page] #all of the places in our db are in @places variable
+    @places = Place.all.page params[:page] 
   end
 
   def new
-    @place = Place.new #Tell the app what we are building the form for.  Build a blank place, so we can
-    #make a form for that particular thing
+    @place = Place.new
   end
 
   def create
-    #pressing submit on the form triggers the create action
-    #all the code here will get executed when the user hits create
-    #create a place connected with this user
     current_user.places.create(place_params)
     redirect_to root_path
   end
 
   private
 
-  def place_params #what pulls the values of name, description and address from the place form
+  def place_params
     params.require(:place).permit(:name, :description, :address)
   end
 
   def show
-    @place = Place.find(params[:id]) #pulls the id out of the URL and uses this to find the specific place
-    #in the db
+    @place = Place.find(params[:id])
   end
 
 end
