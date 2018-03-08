bc57aae39b78ef86a4f6acaaff0956a51708a364
KD33 <Walshk94@gmail.com>
2018-03-08 02:57:24 +0000

    update method added to controller

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -16,7 +16,17 @@ class PlacesController < ApplicationController
   def show
     @place = Place.find(params[:id])
   end
-  
+
+  def edit
+    @place = Place.find(params[:id])
+  end
+
+  def update
+    @place = Place.find(params[:id])
+    @place.update_attributes(place_params)
+    redirect_to root_path
+  end
+
   private
 
   def place_params
