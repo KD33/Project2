6b1dafa6df30b5c7fdfa6737f5d983fccc6ae2b6
KD33 <Walshk94@gmail.com>
2018-03-09 09:23:03 +0000

    Started user validation

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -5,18 +5,27 @@ class PlacesController < ApplicationController
     @places = Place.all.page params[:page]
   end
 
+
   def new
     @place = Place.new
   end
 
+
   def create
-    current_user.places.create(place_params)
-    redirect_to root_path
+    @place = current_user.places.create(place_params)
+    if @place.valid?
+      redirect_to root_path
+    else
+      #render new form and tell browser validation error
+      render :new, status: :unprocessable_entity
+    end
   end
+
 
   def show
     @place = Place.find(params[:id])
   end
+
 
   def edit
     @place = Place.find(params[:id])
@@ -26,15 +35,22 @@ class PlacesController < ApplicationController
     end
   end
 
+
   def update
     @place = Place.find(params[:id])
+
     if @place.user != current_user
       return render plain: 'Not Allowed', status: :forbidden
     end
 
     @place.update_attributes(place_params)
-    redirect_to root_path
+    if @place.valid?
+      redirect_to root_path
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
+
 
   def destroy
     @place = Place.find(params[:id])
@@ -45,6 +61,7 @@ class PlacesController < ApplicationController
     @place.destroy
     redirect_to root_path
   end
+
 
   private
 
