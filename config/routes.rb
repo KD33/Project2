aa5ea5eb8a4b2b77584d8b722d236afebf2d209d
KD33 <Walshk94@gmail.com>
2018-03-11 12:15:14 +0000

    Comment submission form

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root 'places#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :places #gives us CRUD for our places model
+  resources :places do
+    resources :comments, only: :create
+    #gives us CRUD for our places model and C for comments
+  end
 end
